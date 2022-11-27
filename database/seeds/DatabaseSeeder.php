78d161db5de4de6b2cb8a3231e33200eab6ef90d
daduon <da.duon1997@gmail>
2022-11-27 17:15:19 +0700

    update project

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'phone' => '098765432',
+            'password' => Hash::make('admin'),
+        ]);
+    }
+}
